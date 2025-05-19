d40ebb9d80d6135042d7fa6e26fb185e1632f37b
Amélie <amelie.beaufils95@gmail.com>
2025-05-19 14:13:49 +0200

    up 1

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../NavBar";
+
+const DashboardPage = () => {
+    return (
+        <div>
+            <NavBar />
+            <h2>Tableau de bord</h2>
+            <p>Liste des demande de rendez vous</p>
+        </div>
+    );
+}
+
+export default DashboardPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../NavBar";
+
+const HomePage = () => {
+    return (
+        <div>
+            <NavBar />
+            <p>Binvenue sur le site</p>
+        </div>
+    );
+}
+
+export default HomePage;
