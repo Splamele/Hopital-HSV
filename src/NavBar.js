8c9125bec54ca0cf403f56aa11ff86499b26d90c
Amélie <amelie.beaufils95@gmail.com>
2025-05-19 11:36:12 +0200

    first commit

---
+++ src/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    return (
+    <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/book">Reserver</Link></li>
+            <li><Link to="/dashboard">Tableau de bord</Link></li>
+        </ul>
+    </nav>)
+}
+
+export default NavBar;
---
+++ src/components/MyLink.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MyLink = () => {
+    return (
+        <a href="https://www.example.com" target ="_blank">
+            Click here to visit Example.com
+        </a>
+    ); // mettre dans une div si il y a plusieurs éléments 
+}
